505769601929883c7f5149dfa3b68df38e5d3f4f
zerom-H <cupdisin@gmail.com>
2022-08-27 14:59:10 +0900

    QueryDSL Setting & Test

---
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,11 @@
+package study.querydsl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Hello {
+    @Id @GeneratedValue
+    private Long id;
+}
